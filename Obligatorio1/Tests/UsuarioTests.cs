98f0e57750e91944d46838431a76d5ac4a21c910
anaclaravazq9 <anaclvazquez@gmail.com>
2025-04-23 15:31:14 -0300

    GREEN: Contrasena con mayusculas es valida

--- Obligatorio1/Tests/UsuarioTests.cs
+++ Obligatorio1/Tests/UsuarioTests.cs
@@ -50,6 +50,10 @@ namespace Tests
         {
             Usuario usuario1 = new Usuario("Juan", "Perez", "contrasena", "email");
             Assert.IsFalse(usuario1.contrasenaValida());
+            Usuario usuario2 = new Usuario("Juan", "Perez", "Contrasena", "email");
+            Assert.IsTrue(usuario2.contrasenaValida());
+            Usuario usuario3 = new Usuario("Juan", "Perez", "COntrasena", "email");
+            Assert.IsTrue(usuario3.contrasenaValida());
         }
     }
 }
