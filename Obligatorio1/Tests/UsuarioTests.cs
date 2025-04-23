77405b820335e1a92e0f1f6c44e2ca877412dde6
anaclaravazq9 <anaclvazquez@gmail.com>
2025-04-23 15:03:54 -0300

    GREEN: Se suman 2 al agregar otro usuario mas

--- Obligatorio1/Tests/UsuarioTests.cs
+++ Obligatorio1/Tests/UsuarioTests.cs
@@ -20,9 +20,16 @@ namespace Tests
         [TestMethod]
         public void ContadorCantidadUsuarios()
         {
-            Usuario usuario = new Usuario("Juan", "Perez", "contrasena", "email");
+            Usuario usuario1 = new Usuario("Juan", "Perez", "contrasena", "email");
             
             Assert.AreEqual(Usuario.Contador, 1);
+            
+            Usuario usuario2 = new Usuario("Mateo", "Perez", "contrasena", "email");
+            
+            Assert.AreEqual(Usuario.Contador, 2);
         }
+        
+        
+        
     }
 }
