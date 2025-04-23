f361f0427df0614c51924caf464048acd7b7afff
anaclaravazq9 <anaclvazquez@gmail.com>
2025-04-23 15:08:45 -0300

    GREEN: Se asigna correctamente el Id a 1 usuario

--- Obligatorio1/Dominio/Usuario.cs
+++ Obligatorio1/Dominio/Usuario.cs
@@ -3,6 +3,8 @@ namespace Dominio;
 public class Usuario
 {
     public static int Contador { get; set; }
+    
+    public int Id { get; set; }
     public string Nombre { get; set; }
     public string Apellido { get; set; }
     public string Contrasena { get; set; }
@@ -11,6 +13,7 @@ public class Usuario
     public Usuario(string unNombre, string unApellido, string unContrasena, string unEmail)
     {
         Contador++;
+        Id = Contador;
         Nombre = unNombre;
         Apellido = unApellido;
         Contrasena = unContrasena;
