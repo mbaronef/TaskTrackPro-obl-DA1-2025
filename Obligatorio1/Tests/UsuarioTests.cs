e5f605f01ab8fbe34c59aa8c6b3e15ce23f07c92
anaclaravazq9 <anaclvazquez@gmail.com>
2025-04-23 15:23:24 -0300

    RED: No existe el metodo contrasenaValida

--- Obligatorio1/Tests/UsuarioTests.cs
+++ Obligatorio1/Tests/UsuarioTests.cs
@@ -21,11 +21,8 @@ namespace Tests
         public void ContadorCantidadUsuarios()
         {
             Usuario usuario1 = new Usuario("Juan", "Perez", "contrasena", "email");
-            
             Assert.AreEqual(Usuario.Contador, 1);
-            
             Usuario usuario2 = new Usuario("Mateo", "Perez", "contrasena", "email");
-            
             Assert.AreEqual(Usuario.Contador, 2);
         }
         
@@ -48,6 +45,11 @@ namespace Tests
             Assert.AreEqual(usuario2.Id, Usuario.Contador);
         }
 
-
+        [TestMethod]
+        public void ContrasenaIncluyeAlMenosUnaMayuscula()
+        {
+            Usuario usuario1 = new Usuario("Juan", "Perez", "contrasena", "email");
+            Assert.IsFalse(usuario1.contrasenaValida());
+        }
     }
 }
