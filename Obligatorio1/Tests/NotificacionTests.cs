e7c175e687259926e768a15d92079fd172855902
Martina Barone <martinabaronef@gmail.com>
2025-04-24 15:11:24 -0300

    RED: no existe clase Notificacion y constructor

---
+++ Obligatorio1/Tests/NotificacionTests.cs
@@ -0,0 +1,13 @@
+namespace Tests;
+
+[TestClass]
+public class NotificacionTests
+{
+    [TestMethod]
+    public void Constructor_asigna_mensaje_ok()
+    {
+        Notificacion nuevaNotificacion = new Notificacion("Mensaje de notificación");
+        Assert.AreEqual("Mensaje de notificación", nuevaNotificacion.Mensaje);
+    }
+
+}
