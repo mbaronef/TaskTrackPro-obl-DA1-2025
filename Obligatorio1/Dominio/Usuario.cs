1ad3bb9ee61e482eaf10f3ce8ef844fff06196be
Martina Barone <martinabaronef@gmail.com>
2025-04-24 18:51:04 -0300

    REFACTOR: valores por defecto en Usuario. Y eliminación de tests de ID (los gestiona el gestor de usuarios)

--- Obligatorio1/Dominio/Usuario.cs
+++ Obligatorio1/Dominio/Usuario.cs
@@ -2,29 +2,23 @@ namespace Dominio;
 
 public class Usuario
 {
-    public static int Contador { get; set; }
-    
     public int Id { get; set; }
     public string Nombre { get; set; }
     public string Apellido { get; set; }
     public DateTime FechaNacimiento { get; set; }
     public string Email { get; set; }
     public string Contrasena { get; set; }
-    public List<Notificacion> Notificaciones { get; private set; }
-    public bool EsAdministradorProyecto { get; set; }
+    public List<Notificacion> Notificaciones { get; private set; } = new List<Notificacion>();
+    public bool EsAdministradorProyecto { get; set; } =  false;
     
 
     public Usuario(string unNombre, string unApellido, DateTime unaFechaNacimiento, string unEmail, string unaContrasena)
     {
-        Contador++;
-        Id = Contador;
         Nombre = unNombre;
         Apellido = unApellido;
         FechaNacimiento = unaFechaNacimiento;
         Email = unEmail;
         Contrasena = unaContrasena;
-        Notificaciones = new List<Notificacion>();
-        EsAdministradorProyecto = false;
     }
 
     public bool contrasenaValida()
--- Obligatorio1/Tests/UsuarioTests.cs
+++ Obligatorio1/Tests/UsuarioTests.cs
@@ -10,9 +10,9 @@ namespace Tests
         public void ConstructorConParametrosAsignaCorrectamente()
         {
             DateTime fechaNacimiento = new DateTime(2000, 9, 1);
-            
+
             Usuario usuario = new Usuario("Juan", "Perez", fechaNacimiento, "unemail@gmail.com", "Contrase#a3");
-            
+
             Assert.AreEqual("Juan", usuario.Nombre);
             Assert.AreEqual("Perez", usuario.Apellido);
             Assert.AreEqual(fechaNacimiento, usuario.FechaNacimiento);
@@ -24,55 +24,21 @@ namespace Tests
         public void UsuarioSeCreaConListaNotificacionesVacia()
         {
             DateTime fechaNacimiento = new DateTime(2000, 9, 1);
-            
+
             Usuario usuario = new Usuario("Juan", "Perez", fechaNacimiento, "unemail@gmail.com", "Contrase#a3");
-            
+
             Assert.IsNotNull(usuario.Notificaciones);
-            Assert.AreEqual(0,usuario.Notificaciones.Count);
+            Assert.AreEqual(0, usuario.Notificaciones.Count);
         }
-        
+
         [TestMethod]
         public void UnNuevoUsuarioNoEsAdministradorDeProyectoPorDefecto()
         {
             DateTime fechaNacimiento = new DateTime(2000, 9, 1);
-            
+
             Usuario usuario = new Usuario("Juan", "Perez", fechaNacimiento, "unemail@gmail.com", "Contrase#a3");
-            
+
             Assert.IsFalse(usuario.EsAdministradorProyecto);
-        }
-
-        [TestMethod]
-        public void ContadorCantidadUsuarios()
-        {
-            DateTime fechaNacimiento1 = new DateTime(2000, 9, 1);
-            DateTime fechaNacimiento2 = new DateTime(2000, 1, 9);
-            Usuario usuario1 = new Usuario("Juan", "Perez", fechaNacimiento1, "unemail1@gmail.com", "Contrase#a1");
-            Assert.AreEqual(Usuario.Contador, 1);
-            Usuario usuario2 = new Usuario("Mateo", "Perez", fechaNacimiento2, "unemail2@gmail.com", "Contrase#a2");
-            Assert.AreEqual(Usuario.Contador, 2);
-        }
-
-        [TestMethod]
-        public void SeAsigaUnId()
-        {
-            DateTime fechaNacimiento1 = new DateTime(2000, 9, 1);
-            DateTime fechaNacimiento2 = new DateTime(2000, 1, 9);
-            Usuario usuario1 = new Usuario("Juan", "Perez", fechaNacimiento1, "unemail1@gmail.com", "Contrase#a1");
-            Usuario usuario2 = new Usuario("Mateo", "Perez", fechaNacimiento2, "unemail2@gmail.com", "Contrase#a2");
-
-            Assert.AreEqual(usuario1.Id, 1);
-            Assert.AreEqual(usuario2.Id, 2);
-        }
-
-        [TestMethod]
-        public void IdYContadorSeCorresponden()
-        {
-            DateTime fechaNacimiento1 = new DateTime(2000, 9, 1);
-            DateTime fechaNacimiento2 = new DateTime(2000, 1, 9);
-            Usuario usuario1 = new Usuario("Juan", "Perez", fechaNacimiento1, "unemail1@gmail.com", "Contrase#a1");
-            Assert.AreEqual(usuario1.Id, Usuario.Contador);
-            Usuario usuario2 = new Usuario("Mateo", "Perez", fechaNacimiento2, "unemail2@gmail.com", "Contrase#a2");
-            Assert.AreEqual(usuario2.Id, Usuario.Contador);
         }
 
         /*[TestMethod]
