7d31e695c5facd880c4dcb4567489f097787972a
Martina Barone <martinabaronef@gmail.com>
2025-04-24 16:44:54 -0300

    REFACTOR: TestInitialize con creación de notificación y reset del contador static

--- Obligatorio1/Tests/NotificacionTests.cs
+++ Obligatorio1/Tests/NotificacionTests.cs
@@ -3,34 +3,41 @@ namespace Tests;
 using Dominio;
 
 [TestClass]
+[DoNotParallelize]
 public class NotificacionTests
 {
+    private Notificacion _nuevaNotificacion;
+    
+    [TestInitialize]
+    public void SetUp()
+    {
+        // setup para reiniciar la variable estática, sin agregar un método en la clase que no sea coherente con el diseño
+        typeof(Notificacion).GetField("_cantidadNotificaciones", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static).SetValue(null, 0);
+        
+        _nuevaNotificacion = new Notificacion("Mensaje de notificación");
+    }
+    
     [TestMethod]
     public void Constructor_asigna_mensaje_ok()
     {
-        Notificacion nuevaNotificacion = new Notificacion("Mensaje de notificación");
-        Assert.AreEqual("Mensaje de notificación", nuevaNotificacion.Mensaje);
+        Assert.AreEqual("Mensaje de notificación", _nuevaNotificacion.Mensaje);
     }
 
     [TestMethod]
     public void Se_asigna_fecha_ok()
     {
-        Notificacion nuevaNotificacion = new Notificacion("Mensaje de notificación");
-        Assert.AreEqual(DateTime.Today, nuevaNotificacion.Fecha);
+        Assert.AreEqual(DateTime.Today, _nuevaNotificacion.Fecha);
     }
 
     [TestMethod]
     public void Se_asigna_ID_ok()
     {
-        Notificacion nuevaNotificacion = new Notificacion("Mensaje de notificación");
-        Assert.AreEqual(1,nuevaNotificacion.Id);
+        Assert.AreEqual(1,_nuevaNotificacion.Id);
     }
     
     [TestMethod]
     public void Se_asignan_varios_IDs_ok()
     {
-        Notificacion nuevaNotificacion = new Notificacion("Mensaje de notificación");
-        Assert.AreEqual(1,nuevaNotificacion.Id);
         Notificacion segundaNotificacion = new Notificacion("Mensaje de una segunda notificación");
         Assert.AreEqual(2,segundaNotificacion.Id);
         Notificacion terceraNotificacion = new Notificacion("Mensaje de una tercera notificación");
