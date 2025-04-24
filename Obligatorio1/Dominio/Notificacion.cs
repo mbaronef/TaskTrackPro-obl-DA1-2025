e34911f6886794b8729387c3acf287873214b5b8
Martina Barone <martinabaronef@gmail.com>
2025-04-24 15:24:47 -0300

    GREEN: se agregó el atributo Fecha

--- Obligatorio1/Dominio/Notificacion.cs
+++ Obligatorio1/Dominio/Notificacion.cs
@@ -3,9 +3,11 @@ namespace Dominio;
 public class Notificacion
 {
     public string Mensaje { get; set; }
+    public DateTime Fecha { get; private set; }
 
     public Notificacion(string mensaje)
     {
         Mensaje = mensaje;
+        Fecha = DateTime.Today;
     }
 }
