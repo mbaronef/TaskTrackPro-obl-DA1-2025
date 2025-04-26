c0695a6712c9aaf4ad4630f90a081f5b3550b9c7
anaclaravazq9 <anaclvazquez@gmail.com>
2025-04-26 13:32:51 -0300

    RED: No existe la clase tarea, ni estadoTarea

---
+++ Obligatorio1/Tests/TareaTests.cs
@@ -0,0 +1,30 @@
+﻿namespace Tests;
+
+[TestClass]
+
+public class TareaTests
+{
+    [TestMethod]
+
+    public void ConstructorConParametrosAsignadosCorrectamente()
+    {
+        DateTime fechaInicioEstimada = new DateTime(2026, 9, 1);
+        string titulo = "Tarea";
+        string descripcion = "Prueba de tarea";
+        int duracionEnDias = 8;
+        
+        var tarea = new Tarea(titulo, descripcion, duracionEnDias, fechaInicioEstimada);
+        
+        Assert.AreEqual(titulo, tarea.Titulo);
+        Assert.AreEqual(descripcion, tarea.Descripcion);
+        Assert.AreEqual(duracionEnDias, tarea.DuracionEnDias);
+        Assert.AreEqual(fechaInicioEstimada, tarea.FechaInicioMasTemprana);
+        Assert.AreEqual(EstadoTarea.Pendiente, tarea.Estado); // Estado por defecto
+        Assert.IsNotNull(tarea.UsuariosAsignados);
+        Assert.IsNotNull(tarea.DependenciasFF);
+        Assert.IsNotNull(tarea.DependenciasFS);
+        Assert.IsNotNull(tarea.RecursosNecesarios);
+
+    }
+    
+}
