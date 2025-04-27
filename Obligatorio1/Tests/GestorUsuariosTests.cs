16f4b4f7efbe0ce30c92710a36539becd02e588e
Martina Barone <martinabaronef@gmail.com>
2025-04-27 18:56:48 -0300

    REFACTOR: Tests gestorUsuarios

--- Obligatorio1/Tests/GestorUsuariosTests.cs
+++ Obligatorio1/Tests/GestorUsuariosTests.cs
@@ -6,36 +6,54 @@ namespace Tests;
 [TestClass]
 public class GestorUsuariosTests
 {
+    private GestorUsuarios _gestorUsuarios;
+
+    [TestInitialize]
+    public void SetUp()
+    {
+        // setup para reiniciar la variable estática, sin agregar un método en la clase que no sea coherente con el diseño
+        typeof(GestorUsuarios).GetField("_cantidadUsuarios", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static).SetValue(null, 0);
+        
+        _gestorUsuarios = new GestorUsuarios();
+    }
+
+    private Usuario CrearUsuario1()
+    {
+        return new Usuario("Juan", "Perez", new DateTime(2000, 9, 1), "unemail@gmail.com", "Contrase#a3");
+    }
+
+    private Usuario CrearUsuario2()
+    {
+        return new Usuario("Mateo", "Perez", new DateTime(1980, 9, 1), "email@gmail.com", "Contrase#a9");
+    }
+    
     [TestMethod]
     public void ConstructorSinParametrosCreaGestorValido()
-    {
-        GestorUsuarios gestorUsuarios = new GestorUsuarios();
-        Assert.IsNotNull(gestorUsuarios);
-        Assert.AreEqual(0, gestorUsuarios.Usuarios.Count);
+    { 
+        Assert.IsNotNull(_gestorUsuarios);
+        Assert.AreEqual(0, _gestorUsuarios.Usuarios.Count);
     }
 
     [TestMethod]
     public void GestorAgregaUsuariosCorrectamente()
     {
-        GestorUsuarios gestorUsuarios = new GestorUsuarios();
-        Usuario usuario1 = new Usuario("Juan", "Perez", new DateTime(2000, 9, 1), "unemail@gmail.com", "Contrase#a3");
-        gestorUsuarios.AgregarUsuario(usuario1);
-        Usuario usuario2 = new Usuario("Mateo", "Perez", new DateTime(2000, 9, 1), "unemail@gmail.com", "Contrase#a3");
-        gestorUsuarios.AgregarUsuario(usuario2);
+        Usuario usuario1 = CrearUsuario1();
+        Usuario usuario2 = CrearUsuario2();
+        _gestorUsuarios.AgregarUsuario(usuario1);
+        _gestorUsuarios.AgregarUsuario(usuario2);
 
-        Assert.AreEqual(2, gestorUsuarios.Usuarios.Count);
-        Assert.AreSame(usuario1, gestorUsuarios.Usuarios[0]);
-        Assert.AreSame(usuario2, gestorUsuarios.Usuarios[1]);
+        Assert.AreEqual(2, _gestorUsuarios.Usuarios.Count);
+        Assert.AreSame(usuario1, _gestorUsuarios.Usuarios[0]);
+        Assert.AreSame(usuario2, _gestorUsuarios.Usuarios[1]);
     }
 
     [TestMethod]
-    public void GestorLlevaCuentaDeUsuariosCorrectamenteYAsignaIds()
+    public void GestorLlevaCuentaDeUsuariosCorrectamenteYAsignaIdsIncrementales()
     {
-        GestorUsuarios gestorUsuarios = new GestorUsuarios();
-        Usuario usuario1 = new Usuario("Juan", "Perez", new DateTime(2000, 9, 1), "unemail@gmail.com", "Contrase#a3");
-        gestorUsuarios.AgregarUsuario(usuario1);
-        Usuario usuario2 = new Usuario("Mateo", "Perez", new DateTime(2000, 9, 1), "unemail@gmail.com", "Contrase#a3");
-        gestorUsuarios.AgregarUsuario(usuario2);
+        Usuario usuario1 = CrearUsuario1();
+        Usuario usuario2 = CrearUsuario2();
+        _gestorUsuarios.AgregarUsuario(usuario1);
+        _gestorUsuarios.AgregarUsuario(usuario2);
 
         Assert.AreEqual(1, usuario1.Id);
         Assert.AreEqual(2, usuario2.Id);
@@ -44,134 +62,115 @@ public class GestorUsuariosTests
     [TestMethod]
     public void GestorEliminaUsuariosCorrectamente()
     {
-        GestorUsuarios gestorUsuarios = new GestorUsuarios();
-        Usuario usuario1 = new Usuario("Juan", "Perez", new DateTime(2000, 9, 1), "unemail@gmail.com", "Contrase#a3");
-        gestorUsuarios.AgregarUsuario(usuario1);
-        Usuario usuario2 = new Usuario("Mateo", "Perez", new DateTime(2000, 9, 1), "unemail@gmail.com", "Contrase#a3");
-        gestorUsuarios.AgregarUsuario(usuario2);
+        Usuario usuario1 = CrearUsuario1();
+        Usuario usuario2 = CrearUsuario2();
+        _gestorUsuarios.AgregarUsuario(usuario1);
+        _gestorUsuarios.AgregarUsuario(usuario2);
 
-        Assert.AreEqual(2, gestorUsuarios.Usuarios.Count);
-        gestorUsuarios.EliminarUsuario(2);
-        Assert.AreEqual(1, gestorUsuarios.Usuarios.Count);
-
+        _gestorUsuarios.EliminarUsuario(usuario2.Id);
+        Assert.AreEqual(1, _gestorUsuarios.Usuarios.Count);
+        Assert.AreSame(usuario1, _gestorUsuarios.Usuarios[0]);
     }
 
     [TestMethod]
     public void GestorDevuelveUnUsuarioPorId()
     {
-        GestorUsuarios gestorUsuarios = new GestorUsuarios();
-        Usuario usuario1 = new Usuario("Juan", "Perez", new DateTime(2000, 9, 1), "unemail@gmail.com", "Contrase#a3");
-        gestorUsuarios.AgregarUsuario(usuario1);
-        Usuario usuario2 = new Usuario("Mateo", "Perez", new DateTime(2000, 9, 1), "unemail@gmail.com", "Contrase#a3");
-        gestorUsuarios.AgregarUsuario(usuario2);
+        Usuario usuario1 = CrearUsuario1();
+        Usuario usuario2 = CrearUsuario2();
+        _gestorUsuarios.AgregarUsuario(usuario1); 
+        _gestorUsuarios.AgregarUsuario(usuario2); 
 
-        Usuario busqueda = gestorUsuarios.ObtenerUsuario(1);
+        Usuario busqueda = _gestorUsuarios.ObtenerUsuario(usuario1.Id);
         Assert.AreEqual(usuario1, busqueda);
     }
 
     [TestMethod]
     public void GestorAsignaAdministradorDeSistema()
     {
-        GestorUsuarios gestorUsuarios = new GestorUsuarios();
-        Usuario usuario = new Usuario("Juan", "Perez", new DateTime(2000, 9, 1), "unemail@gmail.com", "Contrase#a3");
-        gestorUsuarios.AgregarUsuario(usuario); // usuario con id 1
-
-        gestorUsuarios.AgregarAdministradorSistema(1);
+        Usuario usuario = CrearUsuario1();
+        _gestorUsuarios.AgregarUsuario(usuario);
+        _gestorUsuarios.AgregarAdministradorSistema(usuario.Id);
         Assert.IsTrue(usuario.EsAdministradorSistema);
     }
 
     [TestMethod]
     public void GestorEliminaAdministradorDeSistemaCorrectamente()
     {
-        GestorUsuarios gestorUsuarios = new GestorUsuarios();
         Usuario usuario = new Usuario("Juan", "Perez", new DateTime(2000, 9, 1), "unemail@gmail.com", "Contrase#a3");
-        gestorUsuarios.AgregarUsuario(usuario); // usuario con id 1
-        gestorUsuarios.AgregarAdministradorSistema(1);
-        gestorUsuarios.EliminarAdministradorSistema(1);
+        _gestorUsuarios.AgregarUsuario(usuario);
+        _gestorUsuarios.AgregarAdministradorSistema(usuario.Id);
+        _gestorUsuarios.EliminarAdministradorSistema(usuario.Id);
         Assert.IsFalse(usuario.EsAdministradorSistema);
     }
 
     [TestMethod]
     public void GestorAsignaAdministradorDeProyectoCorrectamente()
     {
-        GestorUsuarios gestorUsuarios = new GestorUsuarios();
+        Usuario usuarioSolicitante = CrearUsuario1();
+        _gestorUsuarios.AgregarUsuario(usuarioSolicitante);
+        _gestorUsuarios.AgregarAdministradorSistema(usuarioSolicitante.Id);
+        Usuario nuevoAdminProyecto = CrearUsuario2();
+        _gestorUsuarios.AgregarUsuario(nuevoAdminProyecto);
         
-        Usuario usuarioSolicitante = new Usuario("Juan", "Perez", new DateTime(2000, 9, 1), "unemail@gmail.com", "Contrase#a3");
-        gestorUsuarios.AgregarUsuario(usuarioSolicitante); // usuario con id 1
-        gestorUsuarios.AgregarAdministradorSistema(1);
-
-        Usuario nuevoAdminProyecto = new Usuario("Mateo", "Perez", new DateTime(2000, 9, 1), "unemail@gmail.com", "Contrase#a4");
-        gestorUsuarios.AgregarUsuario(nuevoAdminProyecto);
-        gestorUsuarios.AsignarAdministradorProyecto(usuarioSolicitante, nuevoAdminProyecto);
-        
+        _gestorUsuarios.AsignarAdministradorProyecto(usuarioSolicitante, nuevoAdminProyecto);
         Assert.IsTrue(nuevoAdminProyecto.EsAdministradorProyecto);
     }
 
     [ExpectedException(typeof(ExcepcionDominio))]
     [TestMethod]
-    public void GestorNoAsginaAdministradorDeProyectoSiLoSolicitaUnNoAdminDeSistema()
+    public void NoAdminSistemaNoPuedeAsignarAdministradorProyecto()
     {
-        GestorUsuarios gestorUsuarios = new GestorUsuarios();
+        Usuario usuarioSolicitante = CrearUsuario1();
+        _gestorUsuarios.AgregarUsuario(usuarioSolicitante);
+        Usuario nuevoAdminProyecto = CrearUsuario2();
+        _gestorUsuarios.AgregarUsuario(nuevoAdminProyecto);
         
-        Usuario usuarioSolicitante = new Usuario("Juan", "Perez", new DateTime(2000, 9, 1), "unemail@gmail.com", "Contrase#a3");
-        gestorUsuarios.AgregarUsuario(usuarioSolicitante);
-        
-        Usuario nuevoAdminProyecto = new Usuario("Mateo", "Perez", new DateTime(2000, 9, 1), "unemail@gmail.com", "Contrase#a4");
-        gestorUsuarios.AgregarUsuario(nuevoAdminProyecto);
-        gestorUsuarios.AsignarAdministradorProyecto(usuarioSolicitante, nuevoAdminProyecto);
+        _gestorUsuarios.AsignarAdministradorProyecto(usuarioSolicitante, nuevoAdminProyecto);
     }
 
     [TestMethod]
     public void GestorEliminaAdministradorDeProyectoCorrectamente()
     {
-        GestorUsuarios gestorUsuarios = new GestorUsuarios();
+        Usuario usuarioSolicitante = CrearUsuario1();
+        _gestorUsuarios.AgregarUsuario(usuarioSolicitante);
+        _gestorUsuarios.AgregarAdministradorSistema(usuarioSolicitante.Id);
+        Usuario nuevoAdminProyecto = CrearUsuario2();
+        _gestorUsuarios.AgregarUsuario(nuevoAdminProyecto);
         
-        Usuario usuarioSolicitante = new Usuario("Juan", "Perez", new DateTime(2000, 9, 1), "unemail@gmail.com", "Contrase#a3");
-        gestorUsuarios.AgregarUsuario(usuarioSolicitante); // usuario con id 1
-        gestorUsuarios.AgregarAdministradorSistema(1);
-
-        Usuario nuevoAdminProyecto = new Usuario("Mateo", "Perez", new DateTime(2000, 9, 1), "unemail@gmail.com", "Contrase#a4");
-        gestorUsuarios.AgregarUsuario(nuevoAdminProyecto);
-        gestorUsuarios.AsignarAdministradorProyecto(usuarioSolicitante, nuevoAdminProyecto);
+        _gestorUsuarios.AsignarAdministradorProyecto(usuarioSolicitante, nuevoAdminProyecto);
+        _gestorUsuarios.EliminarAdministradorProyecto(usuarioSolicitante, nuevoAdminProyecto);
         
-        gestorUsuarios.EliminarAdministradorProyecto(usuarioSolicitante, nuevoAdminProyecto);
         Assert.IsFalse(nuevoAdminProyecto.EsAdministradorProyecto);
     }
 
     [ExpectedException(typeof(ExcepcionDominio))]
     [TestMethod]
-    public void ErrorSiUnNoAdminDeSistemaQuiereEliminarUnAdminDeProyecto()
+    public void  NoAdminSistemaNoPuedeEliminarAdministradorProyecto()
     {
-        GestorUsuarios gestorUsuarios = new GestorUsuarios();
-        
-        Usuario adminSistema = new Usuario("Juan", "Perez", new DateTime(2000, 9, 1), "unemail@gmail.com", "Contrase#a3");
-        gestorUsuarios.AgregarUsuario(adminSistema); // usuario con id 1
-        gestorUsuarios.AgregarAdministradorSistema(1);
-
-        Usuario nuevoAdminProyecto = new Usuario("Mateo", "Perez", new DateTime(2000, 9, 1), "unemail@gmail.com", "Contrase#a4");
-        gestorUsuarios.AgregarUsuario(nuevoAdminProyecto);
-        gestorUsuarios.AsignarAdministradorProyecto(adminSistema, nuevoAdminProyecto);
+        Usuario adminSistema = CrearUsuario1();
+        _gestorUsuarios.AgregarUsuario(adminSistema);
+        _gestorUsuarios.AgregarAdministradorSistema(adminSistema.Id);
+        Usuario nuevoAdminProyecto = CrearUsuario2();
+        _gestorUsuarios.AgregarUsuario(nuevoAdminProyecto);
+        _gestorUsuarios.AsignarAdministradorProyecto(adminSistema, nuevoAdminProyecto);
         
         Usuario usuarioSolicitante = new Usuario("José", "Perez", new DateTime(1999, 9, 1), "unemail@gmail.com", "Contrase#a9");
-        gestorUsuarios.AgregarUsuario(usuarioSolicitante);
+        _gestorUsuarios.AgregarUsuario(usuarioSolicitante);
         
-        gestorUsuarios.EliminarAdministradorProyecto(usuarioSolicitante, nuevoAdminProyecto);
+        _gestorUsuarios.EliminarAdministradorProyecto(usuarioSolicitante, nuevoAdminProyecto);
     }
 
     [ExpectedException(typeof(ExcepcionDominio))]
     [TestMethod]
-    public void ErrorSiSeQuiereElminiarComoAdminProyectoAUnNoAdminDeProyecto()
+    public void ErrorEliminarAdministradorProyectoSiUsuarioNoEsAdministradorProyecto()
     {
-        GestorUsuarios gestorUsuarios = new GestorUsuarios();
+        Usuario usuarioSolicitante = CrearUsuario1();
+        _gestorUsuarios.AgregarUsuario(usuarioSolicitante);
+        _gestorUsuarios.AgregarAdministradorSistema(usuarioSolicitante.Id);
+        Usuario nuevoAdminProyecto = CrearUsuario2();
+        _gestorUsuarios.AgregarUsuario(nuevoAdminProyecto);
         
-        Usuario usuarioSolicitante = new Usuario("Juan", "Perez", new DateTime(2000, 9, 1), "unemail@gmail.com", "Contrase#a3");
-        gestorUsuarios.AgregarUsuario(usuarioSolicitante); // usuario con id 1
-        gestorUsuarios.AgregarAdministradorSistema(1);
-
-        Usuario nuevoAdminProyecto = new Usuario("Mateo", "Perez", new DateTime(2000, 9, 1), "unemail@gmail.com", "Contrase#a4");
-        gestorUsuarios.AgregarUsuario(nuevoAdminProyecto);
-        
-        gestorUsuarios.EliminarAdministradorProyecto(usuarioSolicitante, nuevoAdminProyecto);
+        _gestorUsuarios.EliminarAdministradorProyecto(usuarioSolicitante, nuevoAdminProyecto);
     }
 }
 
