6206f907549dfd68ae2cbe04a82aa04210b9c602
Martina Barone <martinabaronef@gmail.com>
2025-04-27 17:14:28 -0300

    RED: Gestor no elimina usuarios

--- Obligatorio1/Tests/GestorUsuariosTests.cs
+++ Obligatorio1/Tests/GestorUsuariosTests.cs
@@ -8,49 +8,64 @@ public class GestorUsuariosTests
     [TestMethod]
     public void ConstructorSinParametrosCreaGestorValido()
     {
-        GestorUsuarios gestorUsuariosS = new GestorUsuarios();
-        Assert.IsNotNull(gestorUsuariosS);
-        Assert.AreEqual(0,gestorUsuariosS.Usuarios.Count);
-        Assert.AreEqual(0,gestorUsuariosS.AdministradoresSistema.Count);
+        GestorUsuarios gestorUsuarios = new GestorUsuarios();
+        Assert.IsNotNull(gestorUsuarios);
+        Assert.AreEqual(0,gestorUsuarios.Usuarios.Count);
+        Assert.AreEqual(0,gestorUsuarios.AdministradoresSistema.Count);
     }
 
     [TestMethod]
     public void GestorAgregaUsuariosCorrectamente()
     {
-        GestorUsuarios gestorUsuariosS = new GestorUsuarios();
+        GestorUsuarios gestorUsuarios = new GestorUsuarios();
         Usuario usuario1 = new Usuario("Juan", "Perez", new DateTime(2000, 9, 1), "unemail@gmail.com", "Contrase#a3"); 
-        gestorUsuariosS.AgregarUsuario(usuario1);
+        gestorUsuarios.AgregarUsuario(usuario1);
         Usuario usuario2 = new Usuario("Mateo", "Perez", new DateTime(2000, 9, 1), "unemail@gmail.com", "Contrase#a3"); 
-        gestorUsuariosS.AgregarUsuario(usuario2);
+        gestorUsuarios.AgregarUsuario(usuario2);
         
-        Assert.AreEqual(2,gestorUsuariosS.Usuarios.Count);
-        Assert.AreSame(usuario1, gestorUsuariosS.Usuarios[0]);
-        Assert.AreSame(usuario2, gestorUsuariosS.Usuarios[1]);
+        Assert.AreEqual(2,gestorUsuarios.Usuarios.Count);
+        Assert.AreSame(usuario1, gestorUsuarios.Usuarios[0]);
+        Assert.AreSame(usuario2, gestorUsuarios.Usuarios[1]);
     }
 
     [TestMethod]
     public void GestorLlevaCuentaDeUsuariosCorrectamenteYAsignaIds()
     {
-        GestorUsuarios gestorUsuariosS = new GestorUsuarios();
+        GestorUsuarios gestorUsuarios = new GestorUsuarios();
         Usuario usuario1 = new Usuario("Juan", "Perez", new DateTime(2000, 9, 1), "unemail@gmail.com", "Contrase#a3"); 
-        gestorUsuariosS.AgregarUsuario(usuario1);
+        gestorUsuarios.AgregarUsuario(usuario1);
         Usuario usuario2 = new Usuario("Mateo", "Perez", new DateTime(2000, 9, 1), "unemail@gmail.com", "Contrase#a3"); 
-        gestorUsuariosS.AgregarUsuario(usuario2);
+        gestorUsuarios.AgregarUsuario(usuario2);
         
         Assert.AreEqual(1, usuario1.Id);
         Assert.AreEqual(2, usuario2.Id);
     }
 
     [TestMethod]
+    public void GestorEliminaUsuariosCorrectamente()
+    {
+        GestorUsuarios gestorUsuarios = new GestorUsuarios();
+        Usuario usuario1 = new Usuario("Juan", "Perez", new DateTime(2000, 9, 1), "unemail@gmail.com", "Contrase#a3"); 
+        gestorUsuarios.AgregarUsuario(usuario1);
+        Usuario usuario2 = new Usuario("Mateo", "Perez", new DateTime(2000, 9, 1), "unemail@gmail.com", "Contrase#a3"); 
+        gestorUsuarios.AgregarUsuario(usuario2);
+        
+        Assert.AreEqual(2, gestorUsuarios.Usuarios.Count);
+        gestorUsuarios.EliminarUsuario(2);
+        Assert.AreEqual(1, gestorUsuarios.Usuarios.Count);
+
+    }
+
+    [TestMethod]
     public void GestorDevuelveUnUsuarioPorId()
     {
-        GestorUsuarios gestorUsuariosS = new GestorUsuarios();
+        GestorUsuarios gestorUsuarios = new GestorUsuarios();
         Usuario usuario1 = new Usuario("Juan", "Perez", new DateTime(2000, 9, 1), "unemail@gmail.com", "Contrase#a3"); 
-        gestorUsuariosS.AgregarUsuario(usuario1);
+        gestorUsuarios.AgregarUsuario(usuario1);
         Usuario usuario2 = new Usuario("Mateo", "Perez", new DateTime(2000, 9, 1), "unemail@gmail.com", "Contrase#a3"); 
-        gestorUsuariosS.AgregarUsuario(usuario2);
+        gestorUsuarios.AgregarUsuario(usuario2);
         
-        Usuario busqueda = gestorUsuariosS.ObtenerUsuario(1);
+        Usuario busqueda = gestorUsuarios.ObtenerUsuario(1);
         Assert.AreEqual(usuario1, busqueda);
     }
 
