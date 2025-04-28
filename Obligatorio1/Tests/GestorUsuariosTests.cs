6d78e14dec190f20cb2d6ae2e1fc2268cd727659
Inés Gayol <ine@192.168.1.55>
2025-04-28 00:29:40 -0300

    RED: gestorProyectos no existe

---
+++ Obligatorio1/Tests/GestorUsuariosTests.cs
@@ -0,0 +1,26 @@
+using Dominio;
+using Dominio.Excepciones;
+using Dominio.Dummies;
+
+namespace Tests
+
+{
+    [TestClass]
+    public class GestorUsuariosTests
+    {
+        //crearProyecto
+        [TestMethod]
+        public void CrearProyecto_AsignarIdCorrectamente()
+        {
+            Usuario adminSistema = new Usuario();
+            GestorProyectos gestor = new GestorProyectos();
+            List<Usuario> miembros = new List<Usuario>();
+            Proyecto proyecto = new Proyecto("nombre", "descripcion", adminSistema, miembros);
+
+            gestor.CrearProyecto(proyecto, adminSistema);
+
+            Assert.AreEqual(1, proyecto.Id);
+            Assert.AreEqual(proyecto, gestor.Proyectos[0]);
+        }
+    }
+}
