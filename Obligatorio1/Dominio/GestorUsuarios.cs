1644267211e19cdaf29491aeb3783e0f8307a7de
Martina Barone <martinabaronef@gmail.com>
2025-04-27 21:52:46 -0300

    GREEN: Se autogenera una contraseña correctamente

--- Obligatorio1/Dominio/GestorUsuarios.cs
+++ Obligatorio1/Dominio/GestorUsuarios.cs
@@ -1,3 +1,5 @@
+using System.Security.Cryptography;
+using System.Text;
 using Dominio.Excepciones;
 
 namespace Dominio;
@@ -72,4 +74,48 @@ public class GestorUsuarios
         }
         usuarioObjetivo.CambiarContrasena("TaskTrackPro@2025");
     }
+
+    public string AutogenerarContrasena(Usuario administrador, Usuario usuarioObjetivo)
+    {
+        string minusculas = "abcdefghijklmnopqrstuvwxyz";
+        string mayusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        string numeros = "1234567890";
+        string simbolos = "!@#$%^&*()-_=+<>?{}[].,:;¡¿?'/~|°";
+        string todosLosCaracteres = minusculas + mayusculas + numeros + simbolos;
+        
+        StringBuilder contrasenaAutogenerada = new StringBuilder();
+        Random random = new Random();
+        
+        int largo = random.Next(8, 16); // el largo es un número random entre 8 y 15
+        
+        // agregar manualmente una mayúscula, una minúscula, un número y un caracter especial (para asegurar restricciones de contraseña)
+        contrasenaAutogenerada.Append(ObtenerCaracterAleatorio(minusculas, random));
+        contrasenaAutogenerada.Append(ObtenerCaracterAleatorio(mayusculas, random));
+        contrasenaAutogenerada.Append(ObtenerCaracterAleatorio(numeros, random));
+        contrasenaAutogenerada.Append(ObtenerCaracterAleatorio(simbolos, random));
+
+        while (contrasenaAutogenerada.Length < largo)
+            contrasenaAutogenerada.Append(ObtenerCaracterAleatorio(todosLosCaracteres, random)); // agrega a la contraseña caracteres random hasta cumplir longitud
+
+        string nuevaContrasena = MezclarCaracteres(contrasenaAutogenerada.ToString(), random);
+        usuarioObjetivo.CambiarContrasena(nuevaContrasena);
+        return nuevaContrasena; // devuelve la contraseña en texto plano para mostrársela una vez al usuario cuando se genera
+    }
+
+    private static char ObtenerCaracterAleatorio(string caracteres, Random rand) {
+        int indice = rand.Next(0, caracteres.Length);
+        return caracteres[indice]; 
+    } 
+    private static string MezclarCaracteres(string input, Random rand) {
+        char[] array = input.ToCharArray(); // se convierte en array para poder recorrer cada caracter
+        for (int i = array.Length - 1; i > 0; i--) // shuffle
+        {
+            int j = rand.Next(0, i + 1);
+            (array[i], array[j]) = (array[j], array[i]);
+        }
+        return new string(array);
+    }
+
+
+    // refactor :private string GenerarContrasenaValida() y usar RandomNumberGenerator
 }
