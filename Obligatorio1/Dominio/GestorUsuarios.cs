bc361ccf4ed9c66591d55834779b5c9bcc7cefa4
Martina Barone <martinabaronef@gmail.com>
2025-04-27 22:40:55 -0300

    REFACTOR: funciones auxiliares para autogenerarcontraseña con RandomNumberGenerator

--- Obligatorio1/Dominio/GestorUsuarios.cs
+++ Obligatorio1/Dominio/GestorUsuarios.cs
@@ -89,39 +89,46 @@ public class GestorUsuarios
         string todosLosCaracteres = minusculas + mayusculas + numeros + simbolos;
         
         StringBuilder contrasenaAutogenerada = new StringBuilder();
-        Random random = new Random();
         
-        int largo = random.Next(8, 16); // el largo es un número random entre 8 y 15
+        RandomNumberGenerator rng = RandomNumberGenerator.Create(); // generador de números aleatorios criptográficamente seguros
         
+        int largo = ObtenerNumeroAleatorio(8, 15, rng); // el largo es un número random entre 8 y 15
         // agregar manualmente una mayúscula, una minúscula, un número y un caracter especial (para asegurar restricciones de contraseña)
-        contrasenaAutogenerada.Append(ObtenerCaracterAleatorio(minusculas, random));
-        contrasenaAutogenerada.Append(ObtenerCaracterAleatorio(mayusculas, random));
-        contrasenaAutogenerada.Append(ObtenerCaracterAleatorio(numeros, random));
-        contrasenaAutogenerada.Append(ObtenerCaracterAleatorio(simbolos, random));
-
+        contrasenaAutogenerada.Append(ObtenerCaracterAleatorio(minusculas, rng));
+        contrasenaAutogenerada.Append(ObtenerCaracterAleatorio(mayusculas, rng));
+        contrasenaAutogenerada.Append(ObtenerCaracterAleatorio(numeros, rng));
+        contrasenaAutogenerada.Append(ObtenerCaracterAleatorio(simbolos, rng));
+            
         while (contrasenaAutogenerada.Length < largo)
-            contrasenaAutogenerada.Append(ObtenerCaracterAleatorio(todosLosCaracteres, random)); // agrega a la contraseña caracteres random hasta cumplir longitud
-
-        string nuevaContrasena = MezclarCaracteres(contrasenaAutogenerada.ToString(), random);
+            contrasenaAutogenerada.Append(ObtenerCaracterAleatorio(todosLosCaracteres, rng)); // agrega a la contraseña caracteres random hasta cumplir longitud
+            
+        string nuevaContrasena = MezclarCaracteres(contrasenaAutogenerada.ToString(), rng);
         usuarioObjetivo.CambiarContrasena(nuevaContrasena);
         return nuevaContrasena; // devuelve la contraseña en texto plano para mostrársela una vez al usuario cuando se genera
     }
-
-    private static char ObtenerCaracterAleatorio(string caracteres, Random rand) {
-        int indice = rand.Next(0, caracteres.Length);
-        return caracteres[indice]; 
-    } 
-    private static string MezclarCaracteres(string input, Random rand) {
-        char[] array = input.ToCharArray(); // se convierte en array para poder recorrer cada caracter
-        for (int i = array.Length - 1; i > 0; i--) // shuffle
-        {
-            int j = rand.Next(0, i + 1);
-            (array[i], array[j]) = (array[j], array[i]);
-        }
-        return new string(array);
+    private static int ObtenerNumeroAleatorio(int min, int max, RandomNumberGenerator rng)
+    {
+        byte[] buffer = new byte[sizeof(uint)]; // uint: entero de 32 bits sin signo. En el buffer se almacena un número aleatorio.
+        rng.GetBytes(buffer); // llena el buffer con números aleatorios
+        uint numero = BitConverter.ToUInt32(buffer, 0); // se convierte el buffer en número de tipo uint
+        // asegurar que el número esté en el rango
+        int rango = max - min + 1;
+        return (int)(numero % rango) + min;
+    }
+    private static char ObtenerCaracterAleatorio(string caracteres, RandomNumberGenerator rng)
+    {
+        int indice = ObtenerNumeroAleatorio(0, caracteres.Length - 1, rng);
+        return caracteres[indice];
     }
 
-
-    // refactor :private string GenerarContrasenaValida() y usar RandomNumberGenerator
-    // refactor: private void validar solicitante(Usuario usuario, string mensaje)
+    private static string MezclarCaracteres(string input, RandomNumberGenerator rng)
+    {
+        char[] array = input.ToCharArray(); // Convierte la cadena en un array de caracteres para poder recorrerla char a char
+        for (int i = array.Length - 1; i > 0; i--)
+        { //shuffle
+            int j = ObtenerNumeroAleatorio(0, i, rng);
+            (array[i], array[j]) = (array[j], array[i]);
+        }
+        return new string(array); // Convierte el array de nuevo a una cadena
+    }
 }
