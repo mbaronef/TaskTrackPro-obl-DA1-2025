dcf89c265a4a2ea75daa84ecdc02ee563d736c04
Martina Barone <martinabaronef@gmail.com>
2025-04-28 14:32:50 -0300

    RED: no se hizo override de GetHashCode

--- Obligatorio1/Tests/UsuarioTests.cs
+++ Obligatorio1/Tests/UsuarioTests.cs
@@ -8,11 +8,12 @@ namespace Tests
     public class UsuarioTests
     {
         private DateTime _fechaNacimientoValida = new DateTime(2000, 9, 1);
+
         private Usuario CrearUsuarioValido()
         {
-            return new Usuario("Juan", "Perez", _fechaNacimientoValida,  "unemail@gmail.com", "Contrase#a3");
+            return new Usuario("Juan", "Perez", _fechaNacimientoValida, "unemail@gmail.com", "Contrase#a3");
         }
-        
+
         [TestMethod]
         public void ConstructorConParametrosAsignaCorrectamente()
         {
@@ -29,30 +30,30 @@ namespace Tests
         [TestMethod]
         public void ConstructorValidaNombreNoVacio()
         {
-            Usuario usuario = new Usuario("", "Perez", _fechaNacimientoValida,  "unemail@gmail.com", "Contrase#a3");
+            Usuario usuario = new Usuario("", "Perez", _fechaNacimientoValida, "unemail@gmail.com", "Contrase#a3");
         }
-        
+
         [ExpectedException(typeof(ExcepcionDominio))]
         [TestMethod]
         public void ConstructorValidaNombreNoNull()
         {
-            Usuario usuario = new Usuario(null, "Perez", _fechaNacimientoValida,  "unemail@gmail.com", "Contrase#a3");
+            Usuario usuario = new Usuario(null, "Perez", _fechaNacimientoValida, "unemail@gmail.com", "Contrase#a3");
         }
-        
+
         [ExpectedException(typeof(ExcepcionDominio))]
         [TestMethod]
         public void ConstructorValidaApellidoNoVacio()
         {
-            Usuario usuario = new Usuario("Juan", "", _fechaNacimientoValida,  "unemail@gmail.com", "Contrase#a3");
+            Usuario usuario = new Usuario("Juan", "", _fechaNacimientoValida, "unemail@gmail.com", "Contrase#a3");
         }
-        
+
         [ExpectedException(typeof(ExcepcionDominio))]
         [TestMethod]
         public void ConstructorValidaApellidoNoNull()
         {
-            Usuario usuario = new Usuario("Juan", null, _fechaNacimientoValida,  "unemail@gmail.com", "Contrase#a3");
+            Usuario usuario = new Usuario("Juan", null, _fechaNacimientoValida, "unemail@gmail.com", "Contrase#a3");
         }
-        
+
         [TestMethod]
         public void UsuarioSeCreaConListaNotificacionesVacia()
         {
@@ -148,7 +149,7 @@ namespace Tests
             DateTime fechaNacimiento = new DateTime(2020, 1, 6);
             Usuario usuario = new Usuario("Juan", "Perez", fechaNacimiento, "unemail@hotmail.com", "6onTrase}a3");
         }
-        
+
         [TestMethod]
         public void SeCambiaContrasenaCorrectamente()
         {
@@ -157,7 +158,7 @@ namespace Tests
             usuario.CambiarContrasena(nuevaContrasena);
             Assert.IsTrue(usuario.Autenticar(nuevaContrasena));
         }
-        
+
         [ExpectedException(typeof(ExcepcionDominio))]
         [TestMethod]
         public void DaErrorSiSeCambiaContrasenaInvalida()
@@ -176,7 +177,10 @@ namespace Tests
             {
                 usuario.CambiarContrasena(nuevaContrasena);
             }
-            catch { } // Ignorar la excepción
+            catch
+            {
+            } // Ignorar la excepción
+
             Assert.IsFalse(usuario.Autenticar(nuevaContrasena));
             Assert.IsTrue(usuario.Autenticar("Contrase#a3"));
         }
@@ -206,24 +210,26 @@ namespace Tests
             {
                 usuario.CambiarEmail(nuevoEmail);
             }
-            catch (ExcepcionDominio) { } // Ignorar la excepción
-            
+            catch (ExcepcionDominio)
+            {
+            } // Ignorar la excepción
+
             Assert.AreNotEqual(nuevoEmail, usuario.Email);
             Assert.AreEqual("unemail@gmail.com", usuario.Email);
         }
-        
+
         [TestMethod]
         public void SeRecibeUnaNotificacionCorrectamente()
         {
             Usuario usuario = CrearUsuarioValido();
             Notificacion notificacion = new Notificacion("un mensaje de notificación");
-            
+
             usuario.RecibirNotificacion(notificacion);
-            
+
             Assert.AreEqual(1, usuario.Notificaciones.Count);
             Assert.AreSame(notificacion, usuario.Notificaciones.ElementAt(0));
         }
-        
+
         [TestMethod]
         public void SeBorraUnaNotificacionCorrectamente()
         {
@@ -235,7 +241,7 @@ namespace Tests
             usuario.BorrarNotificacion(id);
             Assert.AreEqual(0, usuario.Notificaciones.Count);
         }
-        
+
         [ExpectedException(typeof(ExcepcionDominio))]
         [TestMethod]
         public void BorrarNotificacionInexistenteDaError()
@@ -243,9 +249,9 @@ namespace Tests
             Usuario usuario = CrearUsuarioValido();
             Notificacion notificacion = new Notificacion("un mensaje de notificación");
             usuario.RecibirNotificacion(notificacion); // se agrega notificación con ID 1
-            usuario.BorrarNotificacion(2); 
+            usuario.BorrarNotificacion(2);
         }
-        
+
         [TestMethod]
         public void EqualsRetornaTrueSiLosIdsSonIguales()
         {
@@ -268,7 +274,7 @@ namespace Tests
             bool sonIguales = usuario1.Equals(usuario2);
             Assert.IsFalse(sonIguales);
         }
-        
+
         [TestMethod]
         public void EqualsRetornaFalseSiUnObjetoEsNull()
         {
@@ -276,7 +282,7 @@ namespace Tests
             bool sonIguales = usuario1.Equals(null);
             Assert.IsFalse(sonIguales);
         }
-        
+
         [TestMethod]
         public void EqualsRetornaFalseSiUnObjetoNoEsUsuario()
         {
@@ -285,7 +291,17 @@ namespace Tests
             bool sonIguales = usuario1.Equals(otro);
             Assert.IsFalse(sonIguales);
         }
-        
-        
+
+        [TestMethod]
+        public void GetHashCodeFuncionaOk()
+        {
+            Usuario usuario1 = CrearUsuarioValido();
+            Usuario usuario2 = CrearUsuarioValido();
+            //ambos tienen mismo id ya que no hay un gestor que maneje ids
+            Usuario usuario3 = CrearUsuarioValido();
+            usuario3.Id = 3; // usuario con id distinto a los otros 2 (se hardcodea en vez de llamar al gestor por simplicidad)
+            Assert.AreEqual(usuario1.GetHashCode(), usuario2.GetHashCode());
+            Assert.AreNotEqual(usuario3.GetHashCode(), usuario1.GetHashCode());
+        }
     }
 }
