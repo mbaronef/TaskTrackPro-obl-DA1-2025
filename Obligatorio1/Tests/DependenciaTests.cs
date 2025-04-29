e7f0406ee7bac783c634a9fff9f2d7466b9312bf
anaclaravazq9 <anaclvazquez@gmail.com>
2025-04-29 20:39:12 -0300

    RED: No existe el constructor de dependencia, ni su atributo tipo

---
+++ Obligatorio1/Tests/DependenciaTests.cs
@@ -0,0 +1,23 @@
+﻿namespace Tests;
+using Dominio;
+using Dominio.Excepciones;
+using Dominio.Dummies;
+
+[TestClass]
+
+public class DependenciaTests
+{
+    [TestMethod]
+    public void ConstructorConParametrosAsignadosCorrectamente()
+    {
+        string tipo = "tipo";
+        Tarea tarea = new Tarea("titulo", "descripcion", 6);
+        
+        Dependencia dependencia = new Dependencia(tipo, tarea);
+        
+        Assert.AreEqual(tipo, dependencia.Tipo);
+        Assert.AreEqual(tarea, dependencia.Tarea);
+
+    }
+    
+}
