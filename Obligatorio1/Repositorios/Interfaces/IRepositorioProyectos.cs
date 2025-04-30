504f0b8942b461db8dd7e63b9500ecfe2a324fe8
Martina Barone <martinabaronef@gmail.com>
2025-04-30 20:03:13 -0300

    feat: IRepositorioProyectos

--- Obligatorio1/Repositorios/Interfaces/IRepositorioProyectos.cs
+++ Obligatorio1/Repositorios/Interfaces/IRepositorioProyectos.cs
@@ -4,5 +4,15 @@ namespace Repositorios.IRepositorios;
 
 public interface IRepositorioProyectos : IRepositorio<Proyecto>
 { 
-    
+  void ModificarNombre(int idProyecto, string nombre);
+  void ModificarDescripcion(int idProyecto, string descripcion);
+  void ModificarFechaInicio(int idProyecto, DateTime fechaInicio);
+  void ModificarFechaFinMasTemprana(int idProyecto, DateTime fechaFinMasTemprana);
+  void ModificarAdministrador(int idProyecto, Usuario administrador);
+  void AgregarTarea(int idProyecto, Tarea tarea);
+  void EliminarTarea(int idProyecto, int idTarea);
+  void AgregarMiembro(int idProyecto, Usuario miembro);
+  void EliminarMiembro(int idProyecto, int idMiembro);
+  
+  
 }
