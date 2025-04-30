f42435aa34fb1a07400ffa6c50478b0cf19e8b0a
Martina Barone <martinabaronef@gmail.com>
2025-04-30 20:23:08 -0300

    RED: No se puede agregar usuario al repositorio

--- Obligatorio1/Tests/RepositorioUsuariosTests.cs
+++ Obligatorio1/Tests/RepositorioUsuariosTests.cs
@@ -15,4 +15,15 @@ public class RepositorioUsuariosTests
         Assert.AreEqual("Admin", admin.Apellido);
         Assert.IsTrue(admin.EsAdministradorSistema);
     }
+
+    [TestMethod]
+    public void SeAgregaUnUsuarioOk()
+    {
+        RepositorioUsuarios repositorioUsuarios = new RepositorioUsuarios();
+        Usuario usuario = new Usuario("Juan", "Pérez", new DateTime(1998,7,6), "unEmail@gmail.com", "uNaC@ntr4seña");
+        usuario.Id = 1;
+        repositorioUsuarios.Agregar(usuario);
+        Usuario ultimoDelRepositorioUsuario = repositorioUsuarios.ObtenerPorId(1);
+        Assert.AreEqual(usuario, ultimoDelRepositorioUsuario);
+    }
 }
