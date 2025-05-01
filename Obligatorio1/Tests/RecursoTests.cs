b8eaf72d660706d4fe5cb3e1dd735484d1e21e8b
Martina Barone <martinabaronef@gmail.com>
2025-04-30 22:47:59 -0300

    RED: No existe la clase recurso

---
+++ Obligatorio1/Tests/RecursoTests.cs
@@ -0,0 +1,16 @@
+namespace Tests;
+
+public class RecursoTests
+{
+    [TestMethod]
+    public void ConstructorCreaRecursoYAsignaOk()
+    {
+        Recurso recurso = new Recurso("Nombre", "Tipo", "Descripción");
+        Assert.AreEqual("Nombre",recurso.Nombre);
+        Assert.AreEqual("Tipo", recurso.Tipo);
+        Assert.AreEqual("Descripción", recurso.Descripcion);
+        Assert.IsNull(recurso.ProyectoAsociado);
+        Assert.AreEqual(0,recurso.CantidadDeTareasUsandolo);
+    }
+    
+}
