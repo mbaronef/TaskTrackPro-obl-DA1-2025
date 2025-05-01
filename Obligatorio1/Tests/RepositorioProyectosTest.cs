5644b91b533ecf02095ad92f151e7d781d549398
Martina Barone <martinabaronef@gmail.com>
2025-04-30 21:00:37 -0300

    RED: No existe RepositorioProyectos

---
+++ Obligatorio1/Tests/RepositorioProyectosTest.cs
@@ -0,0 +1,14 @@
+namespace Tests;
+
+[TestClass]
+public class RepositorioProyectosTest
+{
+    [TestMethod]
+    public void ConstructorCreaRepositorioOk()
+    {
+        RepositorioProyectos repositorioProyectos = new RepositorioProyectos();
+        Proyecto proyecto = repositorioProyectos.ObtenerPorId(1);
+        Assert.IsNull(proyecto);
+    }
+    
+}
