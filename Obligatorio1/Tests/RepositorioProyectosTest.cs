c01467b329d28c5b9509d6be9eb3ead7177a3707
Martina Barone <martinabaronef@gmail.com>
2025-04-30 21:12:49 -0300

    RED: No se elimina proyecto

--- Obligatorio1/Tests/RepositorioProyectosTest.cs
+++ Obligatorio1/Tests/RepositorioProyectosTest.cs
@@ -13,5 +13,27 @@ public class RepositorioProyectosTest
         Proyecto proyecto = repositorioProyectos.ObtenerPorId(1);
         Assert.IsNull(proyecto);
     }
-    
+
+    [TestMethod]
+    public void SeAgregaProyectoOk()
+    {
+        RepositorioProyectos repositorioProyectos = new RepositorioProyectos(); 
+        Usuario usuario = new Usuario("Juan", "Pérez", new DateTime(1998,7,6), "unEmail@gmail.com", "uNaC@ntr4seña");;
+        List<Usuario> lista = new List<Usuario>();
+        Proyecto proyecto = new Proyecto("Proyecto", "hacer algo", usuario, lista);
+        repositorioProyectos.Agregar(proyecto);
+        Assert.AreEqual(proyecto, repositorioProyectos.ObtenerPorId(proyecto.Id));
+    }
+
+    [TestMethod]
+    public void SeEliminaProyectoOk()
+    {
+        RepositorioProyectos repositorioProyectos = new RepositorioProyectos(); 
+        Usuario usuario = new Usuario("Juan", "Pérez", new DateTime(1998,7,6), "unEmail@gmail.com", "uNaC@ntr4seña");;
+        List<Usuario> lista = new List<Usuario>();
+        Proyecto proyecto = new Proyecto("Proyecto", "hacer algo", usuario, lista);
+        repositorioProyectos.Agregar(proyecto);
+        repositorioProyectos.Eliminar(proyecto.Id);
+        Assert.IsNull(repositorioProyectos.ObtenerPorId(proyecto.Id));
+    }
 }
