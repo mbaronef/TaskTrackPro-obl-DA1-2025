56d894ec1f2e69f3779add7a8b327e73c9f794ab
Martina Barone <martinabaronef@gmail.com>
2025-05-02 16:08:35 -0300

    RED: No existe la clase gestorRecursos

---
+++ Obligatorio1/Tests/GestorRecursosTests.cs
@@ -0,0 +1,13 @@
+namespace Tests;
+
+[TestClass]
+public class GestorRecursosTests
+{
+    [TestMethod]
+    public void ConstructorCreaGestorValido()
+    {
+        GestorRecursos gestorRecursos = new GestorRecursos();
+        Assert.IsNotNull(gestorRecursos);
+        Assert.AreEqual(0, gestorRecursos.Recursos.Count);
+    }
+}
