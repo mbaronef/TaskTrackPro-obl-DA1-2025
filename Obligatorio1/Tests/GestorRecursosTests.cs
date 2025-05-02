f3f067ae7eb3630d7e9c77f8506440ee052b7b84
Martina Barone <martinabaronef@gmail.com>
2025-05-02 16:37:18 -0300

    RED: No se valida que solo un admin sistema agregue recursos

--- Obligatorio1/Tests/GestorRecursosTests.cs
+++ Obligatorio1/Tests/GestorRecursosTests.cs
@@ -31,18 +31,29 @@ public class GestorRecursosTests
     }
 
     [TestMethod]
-    public void AdminSistemaAgregaUnRecursoCorrectamente()
+    public void AdminSistemaAgregaRecursosCorrectamente()
     {
-        Usuario admin = CrearAdministradorSistema();
+        Usuario adminSistema = CrearAdministradorSistema();
         
         Recurso recurso1 = new Recurso("Analista Senior", "Humano", "Un analista Senior con experiencia");
         Recurso recurso2 = new Recurso("Analista Senior", "Humano", "Un analista Senior con experiencia");
-        _gestorRecursos.AgregarRecurso(admin, recurso1);
-        _gestorRecursos.AgregarRecurso(admin, recurso2);
+        _gestorRecursos.AgregarRecurso(adminSistema, recurso1);
+        _gestorRecursos.AgregarRecurso(adminSistema, recurso2);
         
         Assert.AreEqual(2, _gestorRecursos.Recursos.Count);
         Assert.AreEqual(recurso1, _gestorRecursos.Recursos.ElementAt(0));
         Assert.AreEqual(recurso2, _gestorRecursos.Recursos.ElementAt(1));
+    }
+
+    [ExpectedException(typeof(ExcepcionServicios))]
+    [TestMethod]
+    public void NoAdminSistemaNoAgregaRecurso()
+    {
+        string contrasenaEncriptada = UtilidadesContrasena.ValidarYEncriptarContrasena("Contraseña#3");
+        Usuario usuario = new Usuario("Juan", "Pérez", new DateTime(2000,01,01), "unemail@gmail.com", contrasenaEncriptada);
+        
+        Recurso recurso = new Recurso("Analista Senior", "Humano", "Un analista Senior con experiencia");
+        _gestorRecursos.AgregarRecurso(usuario, recurso);
     }
 
 }
