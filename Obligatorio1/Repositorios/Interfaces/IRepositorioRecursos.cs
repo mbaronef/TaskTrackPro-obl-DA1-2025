b5de47f05d750faf3f070fcc4596f5dae317e801
Martina Barone <martinabaronef@gmail.com>
2025-05-04 01:57:31 -0300

    feat: IRepositorioRecursos

---
+++ Obligatorio1/Repositorios/Interfaces/IRepositorioRecursos.cs
@@ -0,0 +1,12 @@
+using Dominio;
+
+namespace Repositorios.IRepositorios;
+
+public interface IRepositorioRecursos : IRepositorio<Recurso>
+{
+    void ModificarNombre(int idRecurso, string nombre);
+    void ModificarTipo(int idRecurso, string tipo);
+    void ModificarDescripcion(int idRecurso, string descripcion);
+    void ModificarProyectoAsociado(int idRecurso, Proyecto proyecto);
+    void ModificarCantidadDeTareasUsandolo(int idRecurso, int cantidadDeTareasUsandolo);
+}
