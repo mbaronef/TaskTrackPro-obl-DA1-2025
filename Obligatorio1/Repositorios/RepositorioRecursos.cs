f342b2746268c8114cbc767e5cfa36e402737f32
Martina Barone <martinabaronef@gmail.com>
2025-05-04 02:05:25 -0300

    REFACTOR: corrección de test y método

--- Obligatorio1/Repositorios/RepositorioRecursos.cs
+++ Obligatorio1/Repositorios/RepositorioRecursos.cs
@@ -1,6 +1,59 @@
+using Dominio;
+using Repositorios.IRepositorios;
+
 namespace Repositorios;
 
-public class RepositorioRecursos
+public class RepositorioRecursos : IRepositorioRecursos
 {
-    
+    public List<Recurso> _recursos;
+
+    public RepositorioRecursos()
+    {
+        _recursos = new List<Recurso>();
+    }
+
+    public void Agregar(Recurso objeto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Recurso ObtenerPorId(int id)
+    {
+        return _recursos.Find(recurso => recurso.Id == id);
+    }
+
+    public void Eliminar(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Recurso> ObtenerTodos()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void ModificarNombre(int idRecurso, string nombre)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void ModificarTipo(int idRecurso, string tipo)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void ModificarDescripcion(int idRecurso, string descripcion)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void ModificarProyectoAsociado(int idRecurso, Proyecto proyecto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void ModificarCantidadDeTareasUsandolo(int idRecurso, int cantidadDeTareasUsandolo)
+    {
+        throw new NotImplementedException();
+    }
 }
--- Obligatorio1/Tests/RepositorioRecursosTests.cs
+++ Obligatorio1/Tests/RepositorioRecursosTests.cs
@@ -1,6 +1,16 @@
+using Dominio;
+using Repositorios;
+
 namespace Tests;
 
+[TestClass]
 public class RepositorioRecursosTests
 {
-    
+    [TestMethod]
+    public void ConstructorCreaRepositorioOk()
+    {
+        RepositorioRecursos repositorioRecursos = new RepositorioRecursos();
+        Recurso recurso = repositorioRecursos.ObtenerPorId(1);
+        Assert.IsNull(recurso);
+    }
 }
