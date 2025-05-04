db4d15c8c845f22e42cc47700665477690b1264c
Martina Barone <martinabaronef@gmail.com>
2025-05-04 17:23:27 -0300

    GREEN: Se notifica a admin de proyecto la modificación de la descripción de un recurso no exclusivo

--- Obligatorio1/Tests/ServiciosTests/GestorRecursosTests.cs
+++ Obligatorio1/Tests/ServiciosTests/GestorRecursosTests.cs
@@ -491,5 +491,24 @@ public class GestorRecursosTests
         Assert.AreEqual(DateTime.Today, ultimaNotificacion.Fecha);
     }
     
-    //TODO: Si no es exclusivo, se notifica a todos los admin de los proyectos que lo tienen en uso
+    [TestMethod]
+    public void ModificarDescripcionDeRecursoNoExclusivoNotificaAdminDeProyectosQueLoNecesitan()
+    {
+        Usuario adminSistema = CrearAdministradorSistema();
+        Recurso recurso = new Recurso("Analista Senior", "Humano", "Un analista Senior con experiencia");
+        _gestorRecursos.AgregarRecurso(adminSistema, recurso);
+        
+        Usuario adminProyecto = CrearAdministradorProyecto();
+        Proyecto proyecto = new Proyecto("Nombre", "Descripción",DateTime.Today.AddDays(1), adminProyecto, new List<Usuario>());
+        Tarea tarea = new Tarea("Un título", "una descripcion", 3, DateTime.Today.AddDays(10));
+        tarea.AgregarRecurso(recurso);
+        proyecto.AgregarTarea(tarea);
+        _gestorProyectos.CrearProyecto(proyecto, adminProyecto);
+        
+        _gestorRecursos.ModificarDescripcionRecurso(adminSistema, recurso.Id, "Otra descripción");
+        
+        Notificacion ultimaNotificacion = adminProyecto.Notificaciones.Last();
+        Assert.AreEqual("El recurso 'Analista Senior' ha sido modificado. Nuevos valores: Nombre: 'Analista Senior', Tipo: 'Humano', Descripción: 'Otra descripción'.", ultimaNotificacion.Mensaje);
+        Assert.AreEqual(DateTime.Today, ultimaNotificacion.Fecha);
+    }
 }
