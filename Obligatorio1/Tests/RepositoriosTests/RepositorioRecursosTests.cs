4ad960fdeaa9ff9f7edb59b24abd42149a8fb4e6
Inés Gayol <ine@192.168.1.94>
2025-05-04 23:48:06 -0300

    RED: repositorio recursos no tiene metodo agregar recurso

--- Obligatorio1/Tests/RepositoriosTests/RepositorioRecursosTests.cs
+++ Obligatorio1/Tests/RepositoriosTests/RepositorioRecursosTests.cs
@@ -6,11 +6,21 @@ namespace Tests.RepositoriosTests;
 [TestClass]
 public class RepositorioRecursosTests
 {
+    private RepositorioRecursos _repositorioRecursos;
+    private Recurso _recurso;
+    
     [TestMethod]
     public void ConstructorCreaRepositorioOk()
     {
         RepositorioRecursos repositorioRecursos = new RepositorioRecursos();
         Recurso recurso = repositorioRecursos.ObtenerPorId(1);
         Assert.IsNull(recurso);
+    }
+    
+    [TestMethod]
+    public void SeAgregaRecursooOk()
+    {
+        _repositorioRecursos.Agregar(_recurso);
+        Assert.AreEqual(_recurso, _repositorioRecursos.ObtenerPorId(_recurso.Id));
     }
 }
