23aabd79f958b3d3b0ba1d8ed56bc5e947749778
anaclaravazq9 <anaclvazquez@gmail.com>
2025-05-06 09:23:37 -0300

    RED: No existe la clase GestorTareas

---
+++ Obligatorio1/Tests/ServiciosTests/GestorTareasTests.cs
@@ -0,0 +1,26 @@
+﻿using Dominio;
+using Servicios.Excepciones;
+using Servicios.Gestores;
+
+namespace Tests.ServiciosTests;
+
+[TestClass]
+public class GestorTareasTests
+{
+    private GestorTareas _gestorTareas;
+    private GestorProyectos _gestorProyectos;
+    
+    [TestInitialize]
+    public void Inicializar()
+    {
+        _gestorTareas = new GestorTareas();
+        _gestorProyectos = new GestorProyectos();
+    }
+
+    [TestMethod]
+    public void Constructor_CreaGestorValido()
+    {
+        Assert.IsNotNull(_gestorTareas);
+    }
+    
+}
