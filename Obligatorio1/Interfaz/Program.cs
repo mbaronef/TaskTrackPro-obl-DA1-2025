c094759c975d631290248538d664228ce7233c28
Inés Gayol <ine@192.168.1.3>
2025-05-12 00:50:01 -0300

    Recuperar program de integracion logica sesion

--- Obligatorio1/Interfaz/Program.cs
+++ Obligatorio1/Interfaz/Program.cs
@@ -1,17 +1,18 @@
+using Blazored.LocalStorage;
 using Dominio;
 using Interfaz.Components;
 using Interfaz.ServiciosInterfaz;
 using Servicios.Gestores;
+using Servicios.Utilidades;
 
 var builder = WebApplication.CreateBuilder(args);
 
-UsuarioActual usuarioActual = new UsuarioActual();
-Usuario usuario = (new Usuario("Juan", "Pérez", new DateTime(1990,1,1), "admin@gmail.com", "Admin123$"));
-// usuario.EsAdministradorProyecto = true;
+string contrasena = UtilidadesContrasena.ValidarYEncriptarContrasena("Admin123$");
+Usuario usuario = (new Usuario("Juan", "Pérez", new DateTime(1990,1,1), "admin@gmail.com", contrasena));
+usuario.EsAdministradorProyecto = true;
 usuario.EsAdministradorSistema = true;
-usuarioActual.EstablecerUsuario(usuario);
 
-builder.Services.AddSingleton(usuarioActual);
+builder.Services.AddSingleton(usuario);
 
 GestorUsuarios gestorUsuarios = new GestorUsuarios();
 builder.Services.AddSingleton(gestorUsuarios);
@@ -19,18 +20,24 @@ builder.Services.AddSingleton(gestorUsuarios);
 gestorUsuarios.AgregarUsuario(usuario, usuario);
 
 GestorProyectos gestorProyectos = new GestorProyectos();
-// gestorProyectos.CrearProyecto(new Proyecto("Proyecto A", "Descripcion", DateTime.Today.AddDays(1), usuarioActual.UsuarioLogueado, new List<Usuario>()), usuarioActual.UsuarioLogueado);
+gestorProyectos.CrearProyecto(new Proyecto("Proyecto A", "Descripcion", DateTime.Today.AddDays(1), usuario, new List<Usuario>()), usuario);
 builder.Services.AddSingleton(gestorProyectos);
 
 GestorRecursos gestorRecursos = new GestorRecursos(gestorProyectos);
-gestorRecursos.AgregarRecurso(usuarioActual.UsuarioLogueado, new Recurso("Recurso", "tipo", "descripcion"));
+gestorRecursos.AgregarRecurso(usuario, new Recurso("Recurso", "tipo", "descripcion"));
 builder.Services.AddSingleton(gestorRecursos);
 
 
 Usuario usuarioSinRol = gestorUsuarios.CrearUsuario("Sofía", "Martínez", new DateTime(2000, 5, 20), "sofia@gmail.com", "Contrasena123$");
 gestorUsuarios.AgregarUsuario(usuario, usuarioSinRol);
 
+
 // Add services to the container.
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddScoped<LogicaSesion>();
+
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
@@ -39,11 +46,11 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
+    
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
 app.UseHttpsRedirection();
 
 app.UseStaticFiles();
@@ -53,3 +60,11 @@ app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
 app.Run();
+
+
+
+
+
+
+
+
