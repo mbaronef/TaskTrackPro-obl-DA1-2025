87e3f15d584b2fbb859411e931bc633e49a378ad
anaclaravazq9 <anaclvazquez@gmail.com>
2025-05-12 10:40:14 -0300

    REFACTOR: Separacion de clases de DTOs

---
+++ Obligatorio1/Interfaz/DTOs/RecursoDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Interfaz.DTOs;
+
+public class RecursoDTO
+{
+    public int Id { get; set; }
+        
+    [Required(ErrorMessage = "El nombre es obligatorio.")]
+    public string Nombre { get; set; }
+        
+    [Required(ErrorMessage = "El tipo es obligatorio.")]
+    public string Tipo { get; set; }
+        
+    [Required(ErrorMessage = "La descripción es obligatoria.")]
+    public string Descripcion { get; set; }
+}
---
+++ Obligatorio1/Interfaz/DTOs/UsuarioEditarDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Interfaz.DTOs;
+
+public class UsuarioEditarDTO
+{
+    public int Id { get; set; }
+    public string Nombre { get; set; }
+    public string Apellido { get; set; }
+    public DateTime FechaNacimiento { get; set; }
+    public string Email { get; set; }
+
+    public bool EsAdministradorSistema { get; set; }
+    public bool EsAdministradorProyecto { get; set; }
+}
