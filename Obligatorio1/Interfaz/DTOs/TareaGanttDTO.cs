a0ac37c3d17f2683f61f6facf2dec12231c5cc4d
anaclaravazq9 <anaclvazquez@gmail.com>
2025-05-13 14:50:23 -0300

    Diagrama de Gantt con frappe no funciona

---
+++ Obligatorio1/Interfaz/DTOs/TareaGanttDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Interfaz.DTOs;
+
+public class TareaGanttDTO
+{
+    public string id { get; set; } 
+    public string name { get; set; }
+    public string start { get; set; }  // en formato YYYY-MM-DD
+    public string end { get; set; } 
+    public int progress { get; set; }
+    public string dependencies { get; set; }
+}
