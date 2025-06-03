7c9f0abc177cd7b4cc33340d3f7683844af300fc
Inés Gayol <ine@Iness-MacBook-Air.local>
2025-06-03 16:05:56 -0300

    RED: no existe el metodo ObtenerUsuariosDiferentes

--- Obligatorio1/Tests/ControladoresTests/ControladorUsuariosTests.cs
+++ Obligatorio1/Tests/ControladoresTests/ControladorUsuariosTests.cs
@@ -1,4 +1,5 @@
 using Controladores;
+using DTOs;
 using Repositorios;
 using Repositorios.Interfaces;
 using Servicios.Gestores;
@@ -10,18 +11,29 @@ public class ControladorUsuariosTests
 {
     private RepositorioUsuarios _repositorioUsuarios;
     private GestorUsuarios _gestorUsuarios;
+    private ControladorUsuarios _controladorUsuarios;
     
     [TestInitialize]
     public void AntesDeCadaTest()
     {
         _repositorioUsuarios = new RepositorioUsuarios();
         _gestorUsuarios = new GestorUsuarios(_repositorioUsuarios);
+        _controladorUsuarios = new ControladorUsuarios(_gestorUsuarios);
     }
 
     [TestMethod]
     public void Constructor()
     {
-        ControladorUsuarios controlador = new ControladorUsuarios(_gestorUsuarios);
-        Assert.IsNotNull(controlador);
+        Assert.IsNotNull(_controladorUsuarios);
+    }
+    
+    [TestMethod]
+    public void ObtenerUsuariosDiferentes_ObtieneUsuariosOK()
+    {
+        UsuarioListarDTO administrador = UsuarioListarDTO.DesdeEntidad(_gestorUsuarios.AdministradorInicial);
+        List<UsuarioListarDTO> usuarios = new List<UsuarioListarDTO>();
+        usuarios.Add(administrador);
+        List<UsuarioListarDTO> usuariosSinAdministrador = _controladorUsuarios.ObtenerUsuariosDiferentes(usuarios);
+        Assert.AreEqual(0, usuariosSinAdministrador.Count);
     }
 }
