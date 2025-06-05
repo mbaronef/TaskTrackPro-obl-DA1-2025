823e93e5b7166602bf29ea4022556cbe041f654c
Inés Gayol <ine@Iness-MacBook-Air.local>
2025-06-05 13:30:18 -0300

    Comentarios para modificar en proxima rama

--- Obligatorio1/Servicios/Gestores/GestorProyectos.cs
+++ Obligatorio1/Servicios/Gestores/GestorProyectos.cs
@@ -224,4 +224,6 @@ public class GestorProyectos
         Proyecto proyecto = ObtenerProyectoPorId(id); 
         return ProyectoDTO.DesdeEntidad(proyecto);
     }
+    
+    // borrar funciones nuevas, tests nuevos, 
 }
