90d75eea4bb2533429690af4c17dcddf9a25da76
anaclaravazq9 <anaclvazquez@gmail.com>
2025-06-07 16:22:23 -0300

    RED: No existe el controlador de usuarios

---
+++ Obligatorio1/Tests/ControladoresTests/ControladorUsuariosTests.cs
@@ -0,0 +1,20 @@
+﻿using Controladores;
+using DTOs;
+using Servicios.Gestores.Interfaces;
+using Moq;
+
+namespace Tests.ControladoresTests;
+
+public class ControladorUsuariosTests
+{
+    private Mock<IGestorUsuarios> _mockGestorUsuarios;
+    private ControladorUsuarios _controladorUsuarios;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        _mockGestorUsuarios = new Mock<IGestorUsuarios>();
+        _controladorUsuarios = new ControladorUsuarios(_mockGestorUsuarios.Object);
+    }
+    
+}
