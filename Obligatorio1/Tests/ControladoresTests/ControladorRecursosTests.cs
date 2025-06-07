5168a15588c9ff0451112957fc1940aefb7296d4
anaclaravazq9 <anaclvazquez@gmail.com>
2025-06-07 12:37:03 -0300

    RED: No existe la clase ControladorRecursos

---
+++ Obligatorio1/Tests/ControladoresTests/ControladorRecursosTests.cs
@@ -0,0 +1,22 @@
+﻿using Controladores;
+using DTOs;
+using Servicios.Gestores.Interfaces;
+using Moq;
+
+namespace Tests.ControladoresTests;
+
+public class ControladorRecursosTests
+{
+    private Mock<IGestorRecursos> _mockGestorRecursos;
+    private ControladorRecursos _controladorRecursos;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        _mockGestorRecursos = new Mock<IGestorRecursos>();
+        _controladorRecursos = new ControladorTareas(_mockGestorRecursos.Object);
+    }
+    
+    
+    
+}
