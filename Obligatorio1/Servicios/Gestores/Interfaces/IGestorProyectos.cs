47ea62cf1fe8a4a5fdc9afd7a7d3f72c10aec85c
Inés Gayol <ine@192.168.1.7>
2025-06-07 00:43:02 -0300

    Se creo la interfaz IGestorProyectos que la implementa GestorProyectos

---
+++ Obligatorio1/Servicios/Gestores/Interfaces/IGestorProyectos.cs
@@ -0,0 +1,34 @@
+using Dominio;
+using DTOs;
+
+namespace Servicios.Gestores.Interfaces;
+
+public interface IGestorProyectos
+{
+    void CrearProyecto(ProyectoDTO proyectoDTO, UsuarioDTO solicitanteDTO);
+    void EliminarProyecto(int idProyecto, UsuarioDTO solicitanteDTO);
+
+    List<ProyectoDTO> ObtenerTodos();
+    List<Proyecto> ObtenerTodosDominio();
+    ProyectoDTO ObtenerProyectoPorId(int id);
+
+    void ModificarNombreDelProyecto(int idProyecto, string nuevoNombre, UsuarioDTO solicitanteDTO);
+    void ModificarDescripcionDelProyecto(int idProyecto, string descripcion, UsuarioDTO solicitanteDTO);
+    void ModificarFechaDeInicioDelProyecto(int idProyecto, DateTime nuevaFecha, UsuarioDTO solicitanteDTO);
+
+    void CambiarAdministradorDeProyecto(UsuarioDTO solicitanteDTO, int idProyecto, int idNuevoAdmin);
+    void AgregarMiembroAProyecto(int idProyecto, UsuarioDTO solicitanteDTO, UsuarioDTO nuevoMiembroDTO);
+    void EliminarMiembroDelProyecto(int idProyecto, UsuarioDTO solicitanteDTO, int idMiembroAEliminar);
+
+    List<ProyectoDTO> ObtenerProyectosPorUsuario(int idUsuario);
+    Proyecto ObtenerProyectoDelAdministrador(int idAdministrador);
+
+    void VerificarUsuarioNoTieneTareasAsignadas(int idProyecto, int idMiembroAEliminar);
+    void NotificarAdministradoresDeProyectos(List<Proyecto> proyectos, string mensaje);
+
+    void CalcularCaminoCritico(ProyectoDTO proyectoDTO);
+
+    bool EsAdministradorDeProyecto(UsuarioDTO usuarioDTO, int idProyecto);
+    bool EsMiembroDeProyecto(int idUsuario, int idProyecto);
+    Proyecto ObtenerProyectoDominioPorId(int id);
+}
