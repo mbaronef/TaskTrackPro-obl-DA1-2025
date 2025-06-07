b9b0d31dc8946ca97046258bfa79a86282851d4b
Inés Gayol <ine@192.168.1.7>
2025-06-07 00:49:26 -0300

    RED: no esta creado ControladorProyectos

---
+++ Obligatorio1/Tests/ControladoresTests/ControladorProyectosTests.cs
@@ -0,0 +1,25 @@
+namespace Tests.ControladoresTests;
+using DTOs;
+using Servicios.Gestores.Interfaces;
+using Moq;
+
+
+[TestClass]
+public class ControladorProyectosTests
+{
+    private Mock<IGestorProyectos> _mockGestorProyectos;
+    private ControladorProyectos _controladorProyectos;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        _mockGestorProyectos = new Mock<IGestorProyectos>();
+        _controladorProyectos = new ControladorProyectos(_mockGestorProyectos.Object);
+    }
+
+    [TestMethod]
+    public void Constructor_CreaControladorOk()
+    {
+        Assert.IsNotNull(_controladorProyectos);
+    }
+}
