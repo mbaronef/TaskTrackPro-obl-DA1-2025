784cb82d873fd81e38f0efad5c735d319945bf2a
Martina Barone <martinabaronef@gmail.com>
2025-06-06 23:01:14 -0300

    RED: No se llama al gestor en el método modificarDuracionTarea del controlador

--- Obligatorio1/Tests/ControladoresTests/ControladorTareasTests.cs
+++ Obligatorio1/Tests/ControladoresTests/ControladorTareasTests.cs
@@ -23,37 +23,7 @@ public class ControladorTareasTests
     {
         Assert.IsNotNull(_controladorTareas);
     }
-    
-    [TestMethod]
-    public void EsMiembroDeTarea_LLamaCorrectamenteAGestor()
-    {
-        UsuarioDTO usuario = new UsuarioDTO { Id = 1 };
-        int idTarea = 1;
-        int idProyecto = 1;
 
-        _mockGestorTareas.Setup(g => g.EsMiembroDeTarea(usuario, idTarea, idProyecto)).Returns(true);
-
-        bool resultado = _controladorTareas.EsMiembroDeTarea(usuario, idTarea, idProyecto);
-
-        Assert.IsTrue(resultado);
-        _mockGestorTareas.Verify(g => g.EsMiembroDeTarea(usuario, idTarea, idProyecto), Times.Once);
-    }
-
-    [TestMethod]
-    public void ObtenerTareaPorId_LlamaCorrectamenteAGestor()
-    {
-        int idProyecto = 1;
-        int idTarea = 1;
-        TareaDTO tareaEsperada = new TareaDTO { Id = idTarea };
-
-        _mockGestorTareas.Setup(g => g.ObtenerTareaPorId(idProyecto, idTarea)).Returns(tareaEsperada);
-
-        TareaDTO resultado = _controladorTareas.ObtenerTareaPorId(idProyecto, idTarea);
-
-        Assert.AreEqual(tareaEsperada.Id, resultado.Id);
-        _mockGestorTareas.Verify(g => g.ObtenerTareaPorId(idProyecto, idTarea), Times.Once);
-    }
-    
     [TestMethod]
     public void AgregarTareaAlProyecto_LlamaCorrectamenteAGestor()
     {
@@ -69,18 +39,18 @@ public class ControladorTareasTests
     }
     
     [TestMethod]
-    public void CambiarEstadoTarea_LlamaCorrectamenteAGestor()
+    public void ObtenerTareaPorId_LlamaCorrectamenteAGestor()
     {
-        UsuarioDTO usuario = new UsuarioDTO { Id = 1 };
-        int idTarea = 1;
         int idProyecto = 1;
-        EstadoTareaDTO nuevoEstado = EstadoTareaDTO.EnProceso;
-        
-        _mockGestorTareas.Setup(g => g.CambiarEstadoTarea(usuario, idTarea, idProyecto, nuevoEstado));
+        int idTarea = 1;
+        TareaDTO tareaEsperada = new TareaDTO { Id = idTarea };
 
-        _controladorTareas.CambiarEstadoTarea(usuario, idTarea, idProyecto, nuevoEstado);
+        _mockGestorTareas.Setup(g => g.ObtenerTareaPorId(idProyecto, idTarea)).Returns(tareaEsperada);
 
-        _mockGestorTareas.Verify(g => g.CambiarEstadoTarea(usuario, idTarea, idProyecto, nuevoEstado), Times.Once);
+        TareaDTO resultado = _controladorTareas.ObtenerTareaPorId(idProyecto, idTarea);
+
+        Assert.AreEqual(tareaEsperada.Id, resultado.Id);
+        _mockGestorTareas.Verify(g => g.ObtenerTareaPorId(idProyecto, idTarea), Times.Once);
     }
     
     [TestMethod]
@@ -113,11 +83,54 @@ public class ControladorTareasTests
         _mockGestorTareas.Verify(g => g.ModificarDescripcionTarea(usuario, idTarea, idProyecto, nuevaDescripcion), Times.Once);
     }
     
+    [TestMethod]
+    public void ModificarDuracionTarea_LlamaCorrectamenteAGestor()
+    {
+        UsuarioDTO usuario = new UsuarioDTO { Id = 1 };
+        int idTarea = 1;
+        int idProyecto = 1;
+        int nuevaDuracion = 5;
+
+        _mockGestorTareas.Setup(g => g.ModificarDuracionTarea(usuario, idTarea, idProyecto, nuevaDuracion));
+
+        _controladorTareas.ModificarDuracionTarea(usuario, idTarea, idProyecto, nuevaDuracion);
+
+        _mockGestorTareas.Verify(g => g.ModificarDuracionTarea(usuario, idTarea, idProyecto, nuevaDuracion), Times.Once);
+    }
+    
+    [TestMethod]
+    public void CambiarEstadoTarea_LlamaCorrectamenteAGestor()
+    {
+        UsuarioDTO usuario = new UsuarioDTO { Id = 1 };
+        int idTarea = 1;
+        int idProyecto = 1;
+        EstadoTareaDTO nuevoEstado = EstadoTareaDTO.EnProceso;
+        
+        _mockGestorTareas.Setup(g => g.CambiarEstadoTarea(usuario, idTarea, idProyecto, nuevoEstado));
+
+        _controladorTareas.CambiarEstadoTarea(usuario, idTarea, idProyecto, nuevoEstado);
+
+        _mockGestorTareas.Verify(g => g.CambiarEstadoTarea(usuario, idTarea, idProyecto, nuevoEstado), Times.Once);
+    }
+    
+    [TestMethod]
+    public void EsMiembroDeTarea_LLamaCorrectamenteAGestor()
+    {
+        UsuarioDTO usuario = new UsuarioDTO { Id = 1 };
+        int idTarea = 1;
+        int idProyecto = 1;
+
+        _mockGestorTareas.Setup(g => g.EsMiembroDeTarea(usuario, idTarea, idProyecto)).Returns(true);
+
+        bool resultado = _controladorTareas.EsMiembroDeTarea(usuario, idTarea, idProyecto);
+
+        Assert.IsTrue(resultado);
+        _mockGestorTareas.Verify(g => g.EsMiembroDeTarea(usuario, idTarea, idProyecto), Times.Once);
+    }
+    
     
     
     /*métodos a probar:
-    GestorTareas.ModificarDuracionTarea()
-    GestorTareas.ModificarDescripcionTarea()
     GestorTareas.EliminarTareaDelProyecto()
     GestorTareas.AgregarDependenciaATarea()
     GestorTareas.EliminarDependenciaDeTarea()
