8223db5e1da09143afdd1459f7caed38bfe34c88
anaclaravazq9 <anaclvazquez@gmail.com>
2025-06-07 16:59:36 -0300

    RED: No existe el metodo de crear y agregar usuario

--- Obligatorio1/Tests/ControladoresTests/ControladorUsuariosTests.cs
+++ Obligatorio1/Tests/ControladoresTests/ControladorUsuariosTests.cs
@@ -23,4 +23,25 @@ public class ControladorUsuariosTests
     {
         Assert.IsNotNull(_controladorUsuarios);
     }
+    
+    [TestMethod]
+    public void CrearYAgregarUsuario_LlamaCorrectamenteAGestor()
+    {
+        int idProyecto = 1;
+        UsuarioDTO solicitante = new UsuarioDTO { Id = 1 };
+        UsuarioDTO nuevoUsuario = new UsuarioDTO { Id = 2 };
+        
+        _mockGestorUsuarios.Setup(g => g.CrearYAgregarUsuario(solicitante, nuevoUsuario));
+
+        _controladorTareas.CrearYAgregarUsuario(solicitante, nuevoUsuario);
+
+        _mockGestorUsuarios.Verify(g => g.CrearYAgregarUsuario(solicitante, nuevoUsuario), Times.Once);
+    }
+    
+    
+    
+    
+    
+    
+    
 }
