f76cd24f250946d01446591962d5bb3b503f353d
Martina Barone <martinabaronef@gmail.com>
2025-06-15 20:17:31 -0300

    RED: RangoDeUso no tiene ctor con parámetros

--- Obligatorio/Tests/DominioTests/RangoDeUsoTests.cs
+++ Obligatorio/Tests/DominioTests/RangoDeUsoTests.cs
@@ -11,4 +11,20 @@ public class RangoDeUsoTests
         RangoDeUso rango = new RangoDeUso();
         Assert.IsNotNull(rango);
     }
+    
+    [TestMethod]
+    public void ConstructorConParametros_CreaRangoCorrectamente()
+    {
+        DateTime fechaInicio = DateTime.Today;
+        DateTime fechaFin = DateTime.Today.AddDays(10);
+        int capacidadEnUso = 10;
+
+        Tarea tareaQueHaceUso = new Tarea();
+            
+        RangoDeUso rango = new RangoDeUso(fechaInicio, fechaFin, capacidadEnUso, tareaQueHaceUso);
+        Assert.AreEqual(fechaInicio, rango.FechaInicio);
+        Assert.AreEqual(fechaFin, rango.FechaFin);
+        Assert.AreEqual(capacidadEnUso, rango.CantidadDeUsos);
+        Assert.AreEqual(tareaQueHaceUso, rango.TareaQueHaceUso);
+    }
 }
